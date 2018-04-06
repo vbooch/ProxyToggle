bd2dc3bae654d22b7b4ae9d430ea09f60daf8ee2
Booch <reg@booch.info>
2018-04-06 18:14:00 +0300

    Initial file add

---
+++ ProxyToggleExtension/ProxySettingsController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.Win32;
+
+namespace ProxyToggleExtension
+{
+    internal sealed class ProxySettingsController
+    {
+        private void CopyProxySettings(bool enable = true)
+        {
+            const string subKey = "Software\\Microsoft\\Windows\\CurrentVersion\\Internet Settings\\Connections";
+            const string sourceRoot = "HKEY_CURRENT_USER";
+            const string targetRoot = "HKEY_USERS\\S-1-5-18";
+
+            const string sourceKey = sourceRoot + "\\" + subKey;
+            const string targetKey = targetRoot + "\\" + subKey;
+
+            const string parameter = "DefaultConnectionSettings";
+
+            if (Registry.GetValue(sourceKey, parameter, null) is byte[] userSettings)
+            {
+                userSettings[8] = enable ? (byte)3 : (byte)1;
+
+                Registry.SetValue(targetKey, parameter, userSettings);
+            }
+        }
+
+        public void EnableSystemProxy()
+        {
+            CopyProxySettings();
+
+            var psi = new ProcessStartInfo(Environment.SystemDirectory + @"\netsh.exe", "winhttp import proxy source=ie")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            var process = Process.Start(psi);
+            process?.WaitForExit(10 * 1000);
+        }
+
+        public void DisableSystemProxy()
+        {
+            CopyProxySettings(false);
+
+            var psi = new ProcessStartInfo(Environment.SystemDirectory + @"\netsh.exe", "winhttp reset proxy")
+            {
+                CreateNoWindow = true,
+                UseShellExecute = false,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            var process = Process.Start(psi);
+            process?.WaitForExit(10 * 1000);
+        }
+    }
+}
---
+++ ProxyToggleExtension/SystemProxyToggle.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Security.Principal;
+using System.Windows.Forms;
+using Fiddler;
+
+[assembly: RequiredVersion("4.0.0.0")]
+namespace ProxyToggleExtension
+{
+    public sealed class SystemProxyToggle : IFiddlerExtension
+    {
+        private const string ParentMenuIdentifier = "&Rules";
+
+        private MenuItem _rulesParentMenu;
+        private MenuItem _enableMenuItem;
+
+        private const string MenuText = "System Proxy";
+
+        private readonly ProxySettingsController _controller;
+
+        public SystemProxyToggle()
+        {
+            _controller = new ProxySettingsController();
+        }
+
+        public void OnLoad()
+        {
+            _rulesParentMenu = null;
+            foreach (MenuItem menuItem in FiddlerApplication.UI.Menu.MenuItems)
+            {
+                if (string.Equals(menuItem.Text, ParentMenuIdentifier))
+                    _rulesParentMenu = menuItem;
+            }
+
+            if (_rulesParentMenu != null)
+            {
+                var spacer = new MenuItem("-");
+                _rulesParentMenu.MenuItems.Add(spacer);
+
+                _enableMenuItem = new MenuItem(MenuText, Enable_Click);
+                _rulesParentMenu.MenuItems.Add(_enableMenuItem);
+            }
+        }
+
+        public void OnBeforeUnload()
+        {
+            _controller.DisableSystemProxy();
+        }
+
+        private void Enable_Click(object sender, EventArgs e)
+        {
+            var menu = (MenuItem) sender;
+            if (menu == null)
+                return;
+
+            if (!IsAdministrator())
+            {
+                MessageBox.Show("Please launch Fiddler with administrator privileges.");
+                return;
+            }
+
+            menu.Checked = !menu.Checked;
+
+            if (menu.Checked)
+            {
+                //menu.Text = "Disable system proxy";
+                _controller.EnableSystemProxy();
+            }
+            else
+            {
+                //menu.Text = "Enable system proxy";
+                _controller.DisableSystemProxy();
+            }
+        }
+
+        public static bool IsAdministrator()
+        {
+            var identity = WindowsIdentity.GetCurrent();
+            var principal = new WindowsPrincipal(identity);
+            return principal.IsInRole(WindowsBuiltInRole.Administrator);
+        }
+    }
+}
